399eb5583ab6c7523bfe8ffeaaadb13cce8d8f1e
Mathias Schulze <mathias-schulze@web.de>
2024-04-24 22:20:25 +0200

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { DataStore } from 'aws-amplify/datastore';
+import { Todo } from './models';
 
 function App() {
+
+  async function showTodos() {
+    const models = await DataStore.query(Todo);
+    console.log(models);
+  }
+
+  async function addTodo() {
+    await DataStore.save(
+      new Todo({
+        "name": "Lorem ipsum dolor sit amet",
+        "description": "Lorem ipsum dolor sit amet"
+      })
+    );
+  }
+
+  async function updateTodo() {
+    const original = await DataStore.query(Todo, "980ce83f-e9d2-4754-9e93-7a4ea15bb035");
+    await DataStore.save(Todo.copyOf(original, item => {
+      item.name = `title ${Date.now()}`
+    }));
+  }
+
+  async function deleteTodo() {
+    const modelToDelete = await DataStore.query(Todo, "980ce83f-e9d2-4754-9e93-7a4ea15bb035");
+    DataStore.delete(modelToDelete);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button onClick={addTodo}>Add</button>
+      <button onClick={showTodos}>Show</button>
+      <button onClick={updateTodo}>Update</button>
+      <button onClick={deleteTodo}>Delete</button>
       </header>
     </div>
   );
